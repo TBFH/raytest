9488df9e8e5f9f41237250f09ea6ce27f3e2ba20
austin <2986579627@qq.com>
2025-11-08 20:41:09 +0800

    fix .so dir

--- testserve/__init__.py
+++ testserve/__init__.py
@@ -1,9 +1,10 @@
 import os
 import torch
 
-BASE_DIR = os.path.dirname(__file__)
+# BASE_DIR = os.path.dirname(__file__)
 # LIB_DIR = os.path.join(BASE_DIR, "../SwiftTransformer/build/lib")
-LIB_DIR = os.path.join(BASE_DIR, "../build/lib_v2")
+# LIB_DIR = os.path.join("BASE_DIR", "../build/lib_v2")
+LIB_DIR = os.path.join(os.path.expanduser("~"), "libst_pybinding.so")
 
 if not os.path.exists(LIB_DIR):
     raise RuntimeError(
@@ -11,7 +12,8 @@ if not os.path.exists(LIB_DIR):
         "Please build the SwiftTransformer library first or put it at the right place."
     )
 
-torch.ops.load_library(os.path.join(LIB_DIR, "libst_pybinding.so"))
+# torch.ops.load_library(os.path.join(LIB_DIR, "libst_pybinding.so"))
+torch.ops.load_library(LIB_DIR)
 
 from testserve.llm import OfflineLLM
 from testserve.request import SamplingParams
